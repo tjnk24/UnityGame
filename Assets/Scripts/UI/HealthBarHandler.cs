256e327f8fdfe516fca5dd6bcff5e2e2459e5bd8
Tjnk <24>
2020-01-23 23:50:09 +0300

    m

---
+++ Assets/Scripts/UI/HealthBarHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarHandler : MonoBehaviour
+{
+    [SerializeField] private PlayerHealthUI healthBar;
+
+    void Start()
+    {
+        healthBar.SetSize(.4f);
+    }
+}
---
+++ Assets/Scripts/UI/PlayerHealthUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthUI : MonoBehaviour
+{
+    private Transform bar;
+
+    void Start()
+    {
+        bar = transform.Find("Bar");
+    }
+
+    public void SetSize(float sizeNormalized)
+    {
+        bar.localScale = new Vector3(sizeNormalized, 1f);
+    }
+}
